e8330134c01fd5603c73315034800d15bb975d6d
Dihan Official <84884434+dihanofficial@users.noreply.github.com>
2021-07-07 18:37:23 +0530

    Image Editor

---
+++ plugins/commands.py
@@ -0,0 +1,82 @@
+# By @dihanofficial
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram import Client, filters
+from script import script  # pylint:disable=import-error
+
+
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(client: Client, message: Message):
+    try:
+        await message.reply_text(
+            text=script.START_MSG.format(message.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("HELP", callback_data="help_data"),
+                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Updates Channel",
+                            url="https://t.me/dihanofficial",
+                        )
+                    ],
+                ]
+            ),
+            reply_to_message_id=message.message_id,
+        )
+    except Exception:
+        pass
+
+
+@Client.on_message(filters.command(["help"]) & filters.private)
+async def help(client, message):
+    try:
+        await message.reply_text(
+            text=script.HELP_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("BACK", callback_data="start_data"),
+                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Updates Channel",
+                            url="https://t.me/dihanofficial",
+                        )
+                    ],
+                ]
+            ),
+            reply_to_message_id=message.message_id,
+        )
+    except Exception:
+        pass
+
+
+@Client.on_message(filters.command(["about"]) & filters.private)
+async def about(client, message):
+    try:
+        await message.reply_text(
+            text=script.ABOUT_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("BACK", callback_data="help_data"),
+                        InlineKeyboardButton("START", callback_data="start_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Updates Channel",
+                            url="https://t.me/dihanofficial",
+                        )
+                    ],
+                ]
+            ),
+            reply_to_message_id=message.message_id,
+        )
+    except Exception:
+        pass
