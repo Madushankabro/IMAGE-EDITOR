f3cad00a3ff6e9a10a6aa4d89f627cc0aff8dd53
Dihan Official <84884434+dihanofficial@users.noreply.github.com>
2021-07-07 16:59:49 +0530

    Image Editor

---
+++ bot.py
@@ -0,0 +1,22 @@
+# By @DihanRandila
+from pyrogram import Client
+import os
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config  # pylint:disable=import-error
+
+
+if __name__ == "__main__":
+    plugins = dict(root="plugins")
+
+    app = Client(
+        "Image-Editor-DihanOfficial",
+        bot_token=Config.TG_BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins,
+        workers=300,
+    )
+    app.run()
