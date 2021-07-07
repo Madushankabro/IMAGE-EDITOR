cfd35f7e514d0ebe7f127b0788b7458106ac31ba
Dihan Official <84884434+dihanofficial@users.noreply.github.com>
2021-07-07 16:54:44 +0530

    Image Editor Bot😘

---
+++ script.py
@@ -0,0 +1,22 @@
+class script(object):
+
+    START_MSG = """ <b>Hi {}
+
+I'm a Image Editor Bot which Supports various modes
+
+For more click help....</b>"""
+    HELP_MSG = """Hai, Follow these Steps..
+
+<code>🌀 Send me any Image to Edit..</code>
+
+<code>🌀 Select the Corresponding mode that you need</code>
+
+<code>🌀 Your Edited Image will be Uploaded </code>
+
+© @dihanofficial"""
+
+    ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
+
+⭕️<b>Language :</b> <code>Python3</code>
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
+⭕️<b>Group :</b> 👉 <a href='https://t.me/dihan_official'>Click Here</a>"""
