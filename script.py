068329146c31a6728c95abf7bd3ca058a6c6addc
sadew jayasekara <84616471+sadew451@users.noreply.github.com>
2021-07-08 13:01:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -13,10 +13,10 @@ For more click help....</b>"""
 
 <code>🌀 Your Edited Image will be Uploaded </code>
 
-© @dihanofficial"""
+© @SDBOTs_inifinity"""
 
     ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
-⭕️<b>Group :</b> 👉 <a href='https://t.me/dihan_official'>Click Here</a>"""
+⭕️<b>Group :</b> 👉 <a href='https://t.me/SDBOTz'>Click Here</a>"""
