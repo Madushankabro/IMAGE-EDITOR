2e750f27e79335548464c5c394c818131a6b3db8
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-08-09 21:23:51 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -39,7 +39,7 @@ async def photo(client: Client, message: Message):
                         ),
                     ],
                     [
-                        InlineKeyboardButton(text="CLOSE", callback_data="close_e"),
+                        InlineKeyboardButton(text="🔐 CLOSE 🔐", callback_data="close_e"),
                     ],
                 ]
             ),
